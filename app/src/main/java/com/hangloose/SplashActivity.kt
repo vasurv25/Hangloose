1dde29a095befc33f42e94a1d395db1faaf1093d
Vasudevr <vasudevr@cybage.com>
2018-10-29 18:17:31 +0530

    Initial commit

---
+++ app/src/main/java/com/hangloose/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.hangloose
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
