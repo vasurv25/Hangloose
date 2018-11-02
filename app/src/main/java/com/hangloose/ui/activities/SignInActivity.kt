5a861830690dec20ec87986f76be0f1ac2f2088b
anjanim <anjanim@cybage.com>
2018-11-02 17:49:23 +0530

    Sign In UI Started

---
+++ app/src/main/java/com/hangloose/ui/activities/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.hangloose.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hangloose.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
