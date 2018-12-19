3205cd8b7ad696f5f6b656302e37eb7ed445999c
anjanim <anjanim@cybage.com>
2018-12-19 14:24:01 +0530

    - Location Search

--- app/src/main/java/com/hangloose/ui/fragment/RestaurantFragment.kt
+++ app/src/main/java/com/hangloose/ui/fragment/RestaurantFragment.kt
@@ -1,14 +1,25 @@
 package com.hangloose.ui.fragment
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import co.ceryle.radiorealbutton.RadioRealButtonGroup
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.places.ui.PlaceAutocomplete
 import com.hangloose.R
 import com.hangloose.ui.activities.SwipeableCardView
 import com.hangloose.ui.activities.TabsActivity
@@ -19,6 +30,8 @@ import com.hangloose.utils.getDisplaySize
 import com.mindorks.placeholderview.SwipeDecor
 import com.mindorks.placeholderview.SwipePlaceHolderView
 import com.mindorks.placeholderview.SwipeViewBuilder
+import kotlinx.android.synthetic.main.fragment_restaurant.*
+import kotlinx.android.synthetic.main.fragment_restaurant.view.*
 
 class RestaurantFragment : Fragment() {
     private var TAG = "RestaurantFragment"
@@ -26,20 +39,100 @@ class RestaurantFragment : Fragment() {
     private var mBtSearch: Button? = null
     private var mBtRadioRealGroup: RadioRealButtonGroup? = null
     private var mRestaurantData: ArrayList<RestaurantData>? = ArrayList()
+    private val REQUEST_LOCATION_MAPS = 9
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private val LOCATION_REQUEST_CODE = 109
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mRestaurantData = arguments!!.getParcelableArrayList(KEY_DATA)
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!)
+        checkLocationPermission()
         Log.i(TAG, "Restaurant data : $mRestaurantData")
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView = inflater!!.inflate(R.layout.fragment_restaurant, null)
+        val rootView = inflater.inflate(R.layout.fragment_restaurant, null)
         mSwipePlaceHolderView = rootView!!.findViewById(R.id.swipeView) as SwipePlaceHolderView
         mBtSearch = rootView.findViewById(R.id.btSearch) as Button
         mBtRadioRealGroup = rootView.findViewById(R.id.segmentedButtonGroup) as RadioRealButtonGroup
         setSwipeableView()
+        setLocationSearch(rootView)
         return rootView
+    }
+
+    private fun setLocationSearch(rootView: View) {
+        rootView.editLocation.setOnTouchListener(View.OnTouchListener { _, event ->
+            val DRAWABLE_END = 2
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= (rootView.editLocation.right - rootView.editLocation.compoundDrawables[DRAWABLE_END].bounds.width())) {
+                    //Open Maps
+                    val intent = PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                        .build(activity)
+                    startActivityForResult(intent, REQUEST_LOCATION_MAPS)
+                    return@OnTouchListener true
+                }
+            }
+            false
+        })
+    }
+
+    private fun checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                context!!,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                context!!,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                activity!!,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                LOCATION_REQUEST_CODE
+            )
+        } else {
+            mFusedLocationClient.lastLocation.addOnSuccessListener(activity!!) { location ->
+                if (location != null) {
+                    Log.i(TAG, "${location.latitude} && ${location.longitude}")
+                    val geoCoder = Geocoder(context)
+                    val address = geoCoder.getFromLocation(location.latitude, location.longitude, 1)
+                    if (address.size > 0) {
+                        editLocation.setText(address[0].getAddressLine(0), TextView.BufferType.EDITABLE)
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_REQUEST_CODE -> {
+                checkLocationPermission()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_LOCATION_MAPS -> {
+                when (resultCode) {
+                    Activity.RESULT_OK -> {
+                        val place = PlaceAutocomplete.getPlace(context, data)
+                        Log.i(TAG, place.toString())
+                        editLocation.setText(place.address, TextView.BufferType.EDITABLE)
+                    }
+                    PlaceAutocomplete.RESULT_ERROR -> {
+                        val status = PlaceAutocomplete.getStatus(context, data)
+                        Log.i(TAG, status.statusMessage)
+                    }
+                    Activity.RESULT_CANCELED -> Log.i(TAG, "RESULT_CANCELED")
+                }
+            }
+        }
     }
 
     private fun setSwipeableView() {
