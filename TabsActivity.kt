09e04f54602f989c5b81dd1d12ff4ca36aebbcdd
vasurv25 <51511857+vasurv25@users.noreply.github.com>
2019-06-28 18:41:55 +0530

    Add files via upload
    Location code

---
+++ TabsActivity.kt
@@ -0,0 +1,161 @@
+package com.hangloose.ui.activities
+
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.os.Build
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import com.hangloose.R
+import com.hangloose.ui.fragment.*
+import com.hangloose.ui.model.RestaurantData
+import com.hangloose.utils.*
+import com.hangloose.utils.PreferenceHelper.get
+import kotlinx.android.synthetic.main.activity_tab.tabLayout
+
+class TabsActivity : BaseActivity(), TabLayout.OnTabSelectedListener, EnableLocationFragment.LocationListener, RestaurantFragment.LocationNavigationListener {
+
+    private val TAG = "TabsActivity"
+    private var mRestaurantData: ArrayList<RestaurantData>? = null
+
+    private var mPreference: SharedPreferences? = null
+
+    private var mActivitiesSelectedList = ArrayList<String>()
+    private var mAdventuresSelectedList = ArrayList<String>()
+    private val navLabels =
+        intArrayOf(R.string.nav_home, R.string.nav_search, R.string.nav_profile)
+    private val navIcons =
+        intArrayOf(R.drawable.home, R.drawable.search, R.drawable.profile)
+
+    var mHeader : String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab)
+
+        mPreference = PreferenceHelper.defaultPrefs(this)
+        mRestaurantData = intent.getParcelableArrayListExtra(KEY_RESTAURANT_DATA)
+        mActivitiesSelectedList = intent.getStringArrayListExtra(KEY_ACTIVITIES_LIST)
+        mAdventuresSelectedList = intent.getStringArrayListExtra(KEY_ADVENTURES_LIST)
+        val headerToken: String? = mPreference!![X_AUTH_TOKEN]
+        Log.i(TAG, """Header : $headerToken""")
+        mHeader = headerToken
+        Log.i(TAG, "Restaurant data : $mRestaurantData")
+        onTabSelected(tabLayout.getTabAt(0))
+    }
+
+    @SuppressLint("NewApi")
+    override fun init() {
+        (0 until navLabels.size).forEach { i ->
+            val tab = LayoutInflater.from(this).inflate(R.layout.custom_tab, null)
+            val tabLabel = tab.findViewById(R.id.tabText) as TextView
+            val tabIcon = tab.findViewById(R.id.tabIcon) as ImageView
+            tabLabel.text = getString(navLabels[i])
+            /* if (i == 0) {
+                 tabLabel.setTextColor(resources.getColor(R.color.colorPrimary, null))
+                 tabIcon.setColorFilter(Color.parseColor("#b72125"), PorterDuff.Mode.SRC_IN)
+             } else {
+                 tabLabel.setTextColor(resources.getColor(R.color.colorGrey, null))
+             }*/
+            tabIcon.setImageResource(navIcons[i])
+            tabLayout.addTab(tabLayout.newTab().setCustomView(tab))
+        }
+
+        tabLayout.addOnTabSelectedListener(this)
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            tab!!.customView!!.findViewById<ImageView>(R.id.tabIcon)
+                .setColorFilter(Color.parseColor("#cccccc"), PorterDuff.Mode.SRC_IN)
+            tab.customView!!.findViewById<TextView>(R.id.tabText)
+                .setTextColor(resources.getColor(R.color.colorGrey, null))
+        } else {
+            tab!!.customView!!.findViewById<ImageView>(R.id.tabIcon)
+                .setColorFilter(Color.parseColor("#cccccc"), PorterDuff.Mode.SRC_IN)
+            tab.customView!!.findViewById<TextView>(R.id.tabText)
+                .setTextColor(resources.getColor(R.color.colorGrey))
+        }
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        when {
+            tabLayout.selectedTabPosition == 0 -> {
+                replaceFragment(RestaurantFragment())
+            }
+            tabLayout.selectedTabPosition == 1 -> {
+                replaceFragment(SearchFragment())
+            }
+            tabLayout.selectedTabPosition == 2 -> {
+                replaceFragment(ProfileFragment())
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            tab!!.customView!!.findViewById<ImageView>(R.id.tabIcon)
+                .setColorFilter(Color.parseColor("#b72125"), PorterDuff.Mode.SRC_IN)
+            tab.customView!!.findViewById<TextView>(R.id.tabText)
+                .setTextColor(resources.getColor(R.color.colorPrimary))
+        } else {
+            tab!!.customView!!.findViewById<ImageView>(R.id.tabIcon)
+                .setColorFilter(Color.parseColor("#b72125"), PorterDuff.Mode.SRC_IN)
+            tab.customView!!.findViewById<TextView>(R.id.tabText)
+                .setTextColor(resources.getColor(R.color.colorPrimary))
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        Log.i(TAG, "Restaurant data : $mRestaurantData")
+        if (fragment is RestaurantFragment || fragment is SearchFragment) {
+            val args = Bundle()
+            args.putStringArrayList(KEY_ACTIVITIES_LIST, mActivitiesSelectedList)
+            args.putStringArrayList(KEY_ADVENTURES_LIST, mAdventuresSelectedList)
+            args.putParcelableArrayList(KEY_DATA, mRestaurantData)
+            fragment.arguments = args
+        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+    override fun onBackPressed() {}
+
+    override fun onNavigateToRestaurantFragment(restaurantData: ArrayList<RestaurantData>?) {
+        mRestaurantData = restaurantData
+        replaceFragment(RestaurantFragment())
+    }
+
+    override fun navigateToLocationActivityFromHomeFrag() {
+        var intent = Intent(this, LocationSettingActivity::class.java)
+        intent.putStringArrayListExtra(KEY_ACTIVITIES_LIST, mActivitiesSelectedList)
+        intent.putStringArrayListExtra(KEY_ADVENTURES_LIST, mAdventuresSelectedList)
+        intent.putExtra(FLAG_LOCATION_NAVIGATION, 1)
+        startActivityForResult(intent, START_LOCATION_SCREEN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == START_LOCATION_SCREEN) {
+            //var data = data!!.getParcelableArrayListExtra<RestaurantData>(KEY_RESTAURANT_DATA)
+            if (data == null) {
+                Log.d(TAG, "Restaurant Data : $mRestaurantData")
+                replaceFragment(RestaurantFragment())
+            } else {
+                mRestaurantData = data.getParcelableArrayListExtra(KEY_RESTAURANT_DATA)
+                Log.d(TAG, "Restaurant Data : $mRestaurantData")
+                replaceFragment(RestaurantFragment())
+            }
+        }
+    }
+}
---
+++ Utils.kt
@@ -0,0 +1,105 @@
+package com.hangloose.utils
+
+import android.app.Activity
+import android.content.res.Resources
+import android.graphics.Point
+import android.location.Address
+import android.location.Geocoder
+import android.os.Build
+import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import android.widget.Toast
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+
+
+fun showSnackBar(view: View, msg: String, color: Int, bgColor: Int) {
+    val snackbar = Snackbar.make(
+        view, msg,
+        Snackbar.LENGTH_LONG
+    )
+    val snackbarView = snackbar.view
+    snackbarView.setBackgroundColor(bgColor)
+    val textView =
+        snackbarView.findViewById(android.support.design.R.id.snackbar_text) as TextView
+    textView.setTextColor(color)
+    textView.textSize = 14f
+    snackbar.show()
+}
+
+fun hideSoftKeyboard(activity: Activity) {
+    val inputMethodManager = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(activity.currentFocus!!.windowToken, 0)
+}
+
+fun validatePhoneNumber(phoneNumber: String?): Boolean {
+    var isValid = !phoneNumber.isNullOrEmpty()
+    if (isValid) {
+        isValid = phoneNumber!!.length == 10
+    }
+    return isValid
+}
+
+fun validatePassword(password: String?): Boolean {
+    var isValid = !password.isNullOrEmpty()
+    if (isValid) {
+        isValid = password!!.length >= 6
+    }
+    return isValid
+}
+
+fun validateConfirmPassword(password: String?, confirmPassword: String?): Boolean {
+    var isValid = !confirmPassword.isNullOrEmpty()
+    if (isValid) {
+        isValid = password == confirmPassword
+    }
+    return isValid
+}
+
+fun requestPermissionForOtp(activity: Activity) {
+    ActivityCompat.requestPermissions(
+        activity,
+        arrayOf(android.Manifest.permission.RECEIVE_SMS),
+        REQUEST_PERMISSIONS
+    )
+}
+
+fun getDisplaySize(windowManager: WindowManager): Point {
+    try {
+        if (Build.VERSION.SDK_INT > 16) {
+            val display = windowManager.defaultDisplay
+            val displayMetrics = DisplayMetrics()
+            display.getMetrics(displayMetrics)
+            return Point(displayMetrics.widthPixels, displayMetrics.heightPixels)
+        } else {
+            return Point(0, 0)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        return Point(0, 0)
+    }
+}
+
+fun dpToPx(dp: Int): Int {
+    return (dp * Resources.getSystem().displayMetrics.density).toInt()
+}
+
+fun getLatLongFromLocationName(activity: Activity, address: String?): Address? {
+    var location: Address? = null
+    try {
+        var geoCoder = Geocoder(activity)
+        var list = geoCoder.getFromLocationName(address, 1)
+        location = list[0]
+    } catch (e : IOException) {
+        e.printStackTrace()
+        Toast.makeText(activity, "Location Not found", Toast.LENGTH_SHORT).show()
+    }
+    return location
+}
