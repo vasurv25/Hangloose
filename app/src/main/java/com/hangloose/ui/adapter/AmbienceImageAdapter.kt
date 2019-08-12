56d0a6a1b02ee525a28b9d70322a4dd5c59e0c49
vasudev rao <vasudevr@cybage.com>
2019-08-12 17:19:47 +0530

    1. Minor change xml visibility.

---
+++ app/src/main/java/com/hangloose/ui/adapter/AmbienceImageAdapter.kt
@@ -0,0 +1,39 @@
+package com.hangloose.ui.adapter
+
+import android.content.Context
+import android.support.v4.view.PagerAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.hangloose.R
+import kotlinx.android.synthetic.main.ambience_image_item.view.*
+
+class AmbienceImageAdapter(val context: Context, val imageList: ArrayList<String>) :
+    PagerAdapter() {
+
+    private var TAG = "AmbienceImageAdapter"
+    private var layoutInflater: LayoutInflater? = null
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view == `object`
+    }
+
+    override fun getCount(): Int {
+        return imageList.size
+    }
+
+    override fun instantiateItem(parent: ViewGroup, position: Int): Any {
+        layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val itemView = layoutInflater!!.inflate(R.layout.ambience_image_item, parent, false)
+        Glide.with(context!!).load(imageList[position]).into(itemView.ivAmbience)
+        parent.addView(itemView.ivAmbience)
+        return itemView
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        super.destroyItem(container, position, `object`)
+        container.removeView(`object` as View?)
+    }
+}
